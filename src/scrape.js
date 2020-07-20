76cfbe71cf9db0b01b700a3911c00aa1b79cd206
rambhatm <rammulky@gmail.com>
2020-07-20 08:41:18 +0530

    scra

--- src/scrape.js
+++ src/scrape.js
@@ -4,8 +4,9 @@ const Stock = require('./models/stock_model')
 const axios = require('axios').default
 const csv = require('@fast-csv/parse');
 
+const wsb100 = ['tsla', 'snap', 'amzn', 'aapl', 'msft']
 
-const getStocks = async () => {
+async function getStocks() {
     try {
         resp = await axios.get("https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download")
 
@@ -14,21 +15,42 @@ const getStocks = async () => {
                 throw error
             })
             .on('data', (row) => {
-                let stock = new Stock({
-                    Symbol: row.Symbol,
-                    Name: row.Name,
-                    LastSale: row.LastSale,
-                    MarketCap: row.MarketCap,
-                    IPOyear: row.IPOyear,
-                    Sector: row.Sector,
-                    Industry: row.industry,
-                    URL: row.Summary
-                })
-                stock.save()
+                let symbol = row.Symbol.toLowerCase()
+                if (wsb100.indexOf(symbol) != -1) {
+                    let stock = new Stock({
+                        Symbol: symbol,
+                        Name: row.Name,
+                        LastSale: row.LastSale,
+                        MarketCap: row.MarketCap,
+                        IPOyear: row.IPOyear,
+                        Sector: row.Sector,
+                        Industry: row.industry,
+                        URL: row.Summary
+                    })
+                    stock.save()
+                }
             })
             .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
     } catch (error) {
 
     }
+    return
 }
+
+async function getQuote(symbol) {
+    url = `https://api.tiingo.com/tiingo/daily/${symbol}/prices`
+    options = { "headers": { "Authorization": process.env.T_KEY } }
+
+    try {
+        resp = await axios.get(url, options)
+
+    } catch (error) {
+        error.stack
+
+    }
+
+}
+
+
+
 getStocks()
