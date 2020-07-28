212ae65ba4d61226f90da2d28537be4e6f9c1758
Ram Bhat <rammulky@gmail.com>
2020-07-28 13:53:36 +0530

    before and after

--- test/core/coreTest.js
+++ test/core/coreTest.js
@@ -1,13 +1,22 @@
 const should = require('chai').should
 const assert = require('chai').assert
+const mongoose = require('mongoose')
 const core = require('../../core/core.js')
 const scrappy = require('../../sCrappy/scrappy')
+const { before } = require('mocha')
 
 
 describe("core", async function () {
-    it("Init stocks ", async function() {
+    process.env.STOCK_DB += '_test'
+    before(async function(){
         let numRowsParsed = await scrappy.initStocks()
         assert.notEqual(numRowsParsed, 0)
+    })
+
+    after(async function(){
+        let conn = await mongoose.createConnection(process.env.STOCK_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+        await conn.dropDatabase()
+
     })
 
     it("Gets stock of TSLA", async function () {
